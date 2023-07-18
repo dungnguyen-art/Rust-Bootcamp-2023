f31c67bc78787cf0a5f37b164bf89b17ed1767e1
nguyentiendung <nguyentiendung@kali.dungnguyen>
2023-07-18 18:32:24 +0700

    feat: Update hw class-4 generic type

--- exercises/generic-type/src/lib.rs
+++ exercises/generic-type/src/lib.rs
@@ -1,4 +1,4 @@
-// Exercise 1 
+// Exercise 1
 // Implement struct Point to make it work.
 // Make it compile
 fn exercise1() {
@@ -6,7 +6,10 @@ fn exercise1() {
     let float = Position { x: 1.0, y: 4.0 };
 }
 
-
+struct Position<U, T> {
+    x: U,
+    y: T,
+}
 
 // Exercise 2
 // Modify this struct to make the code work
@@ -18,28 +21,30 @@ struct Point<T> {
 
 fn exercise2() {
     // DON'T modify this code.
-    let p = Point{x: 5, y : "hello".to_string()};
+    let p = Point {
+        x: 5.to_string(),
+        y: "hello".to_string(),
+    };
 }
-
-
 
 // Exercise 3
 // Make it compile
 // Add generic for Val to make the code work, DON'T modify the code in `main`.
-struct Val {
-    val: f64,
+struct Val<T> {
+    val: T,
 }
 
-impl Val {
-    fn value(&self) -> &f64 {
+impl<T> Val<T> {
+    fn value(&self) -> &T {
         &self.val
     }
 }
 
-
 fn exercise3() {
-    let x = Val{ val: 3.0 };
-    let y = Val{ val: "hello".to_string()};
+    let x = Val { val: 3.0 };
+    let y = Val {
+        val: "hello".to_string(),
+    };
     println!("{}, {}", x.value(), y.value());
 }
 
@@ -49,23 +54,34 @@ fn exercise3() {
 // Implementing logic
 // Run tests
 
-fn find_max<T>(collection: &[T]) -> Option<&T> {
-    todo!()
+fn find_max<T: std::cmp::PartialOrd>(collection: &[T]) -> Option<&T> {
+    if collection.is_empty() {
+        return None;
+    }
+    let mut max_value = &collection[0];
+    for element in collection {
+        if element > max_value {
+            max_value = element;
+        }
+    }
+    return Some(max_value);
 }
 
 // Exercise 5 
 // Reverse the elements in a collection
 // Make it compile 
 // Run tests 
-fn reverse_collection<T>(collection: &[T]) {
-    todo!()
+fn reverse_collection<T>(collection: &mut [T]) {
+    collection.reverse();
 }
-
 
 // Exercise 6
 // Function to check if a collection contains a specific value
-fn contains_value<T>(collection: &[T], value: &T) -> bool {
-    todo!()
+fn contains_value<T>(collection: &[T], value: &T) -> bool
+where
+    T: PartialEq,
+{
+    collection.contains(value)
 }
 
 // Unit tests
@@ -94,29 +110,29 @@ mod tests {
         assert_eq!(find_max(&empty), None);
     }
 
-    // Test for exercise 5
-    #[test]
-    fn test_reverse_collection_with_numbers() {
-        let mut numbers = vec![1, 2, 3, 4, 5];
-        reverse_collection(&mut numbers);
-        assert_eq!(numbers, vec![5, 4, 3, 2, 1]);
-    }
-
-    // Test for exercise 5
-    #[test]
-    fn test_reverse_collection_with_strings() {
-        let mut strings = vec!["apple", "banana", "cherry", "durian"];
-        reverse_collection(&mut strings);
-        assert_eq!(strings, vec!["durian", "cherry", "banana", "apple"]);
-    }
-
-    // Test for exercise 5
-    #[test]
-    fn test_reverse_collection_with_empty_collection() {
-        let mut empty: Vec<i32> = Vec::new();
-        reverse_collection(&mut empty);
-        assert_eq!(empty, Vec::<i32>::new());
-    }
+     // Test for exercise 5
+     #[test]
+     fn test_reverse_collection_with_numbers() {
+         let mut numbers = vec![1, 2, 3, 4, 5];
+         reverse_collection(&mut numbers);
+         assert_eq!(numbers, vec![5, 4, 3, 2, 1]);
+     }
+ 
+     // Test for exercise 5
+     #[test]
+     fn test_reverse_collection_with_strings() {
+         let mut strings = vec!["apple", "banana", "cherry", "durian"];
+         reverse_collection(&mut strings);
+         assert_eq!(strings, vec!["durian", "cherry", "banana", "apple"]);
+     }
+ 
+     // Test for exercise 5
+     #[test]
+     fn test_reverse_collection_with_empty_collection() {
+         let mut empty: Vec<i32> = Vec::new();
+         reverse_collection(&mut empty);
+         assert_eq!(empty, Vec::<i32>::new());
+     }
 
     // Test for exercise 6
     #[test]
@@ -140,5 +156,4 @@ mod tests {
         let empty: Vec<i32> = Vec::new();
         assert_eq!(contains_value(&empty, &5), false);
     }
-
 }
