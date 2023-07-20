1b934ae9c4ff9cc1fc51d190f14f5f9f907906d8
nguyentiendung <nguyentiendung@kali.dungnguyen>
2023-07-20 23:32:58 +0700

    HW5: error handling

--- exercises/error-handling/src/lib.rs
+++ exercises/error-handling/src/lib.rs
@@ -2,22 +2,27 @@
 // Make it compile in unit test
 // Run tests
 // Hint: Convert Option to Result
-fn generate_nametag_text(name: String) -> Option<String> {
+fn generate_nametag_text(name: String) -> Result<String, String> {
     if name.is_empty() {
         // Empty names aren't allowed.
-        None
+        Err("`name` was empty; it must be nonempty.".into())
     } else {
-        Some(format!("Hi! My name is {}", name))
+        Ok(format!("Hi! My name is {}", name))
     }
 }
 // Exercise 2
 // Make it compile in unit test
 // Run tests
 // Hint: &str to integer conversion by using parse method and return Result
-use std::num::ParseIntError;
+// use std::{num::ParseIntError, fmt::format};
 
-fn parse_number(s: &str) -> Result<i32, ParseIntError> {
-    todo!()
+
+#[derive(Debug, PartialEq)]
+struct CustomError(String);
+
+fn parse_number(s: &str) -> Result<i32, CustomError> {
+    s.parse::<i32>()
+        .map_err(|err| CustomError(err.to_string()))
 }
 
 // Exercise 3
@@ -35,8 +40,13 @@ enum CreationError {
 
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
-        // Hmm...? Why is this only returning an Ok value?
-        Ok(PositiveNonzeroInteger(value as u64))
+        if value > 0 {
+            Ok(PositiveNonzeroInteger(value as u64))
+        } else if value < 0 {
+            Err(CreationError::Negative)
+        } else {
+            Err(CreationError::Zero)
+        }
     }
 }
 
@@ -65,10 +75,9 @@ mod tests {
         assert_eq!(parse_number("42"), Ok(42));
         assert_eq!(
             parse_number("invalid"),
-            Err("invalid digit found in string".parse().unwrap())
+            Err(CustomError("invalid digit found in string".to_string()))
         );
     }
-
     /// Test for exercise 3
     #[test]
     fn exercise3_should_work() {
